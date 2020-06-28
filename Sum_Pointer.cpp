a37f8f93b0997daa5661d3961c131ae6ee9e3ce7
Shrikanth-123 <53474698+Shrikanth-123@users.noreply.github.com>
2020-06-28 20:43:32 +0530

    Create Sum_Pointer.cpp

---
+++ Sum_Pointer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int *pNum1=new int;
+	int *pNum2=new int;
+	int *pAdd=new int;
+	
+	
+	cout<<"Enter first number: ";
+	cin>>(*pNum1);
+	cout<<"Enter second number: ";
+	cin>>(*pNum2);
+	
+	
+	*pAdd=*pNum1+*pNum2;
+	
+	
+	cout<<"Addition is: "<<*pAdd<<endl;
+	
+	return 0;
+}
